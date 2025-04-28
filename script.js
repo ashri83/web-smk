0be3000542d4d7d2dc51cd3c45fad72378bd178e
ashririswandidjamil <ashri83@gmail.com>
2025-04-28 15:39:54 +0700

    Initial commit - Website SMK Pondok Petir

---
+++ script.js
@@ -0,0 +1,10 @@
+fetch('/news/index.json')
+  .then(response => response.json())
+  .then(data => {
+    const list = document.getElementById('list-berita');
+    data.berita.forEach(item => {
+      const article = document.createElement('article');
+      article.innerHTML = `<h3>${item.title}</h3><p>${item.date}</p><p>${item.body}</p>`;
+      list.appendChild(article);
+    });
+  });
